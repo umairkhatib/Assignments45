63558257ec3d71a57ebe0a29f1346c5f46d4ff26
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-18 01:02:32 +0500

    first commit

---
+++ Q5PrintQuote/main.js
@@ -0,0 +1,4 @@
+// Assignment # 5
+var message = "Honesty is the first chapter in the book of wisdom";
+var famous_person = "Thomas Jefferson";
+console.log("\"".concat(message, "\" - ").concat(famous_person));
---
+++ Q5PrintQuote/main.ts
@@ -0,0 +1,6 @@
+// Assignment # 5
+let message : string = "Honesty is the first chapter in the book of wisdom";
+
+let famous_person :string = "Thomas Jefferson";
+
+console.log(`"${message}" - ${famous_person}`);
