4bfda72146668ed37980e7c0f333f2c1a9741076
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-18 21:11:27 +0500

    first commit

---
+++ Q6StrippingNames/main.js
@@ -0,0 +1,5 @@
+// Assignment # Stripping Names: 
+var personName = "\tAli abdullah khatib\n";
+console.log(personName);
+var strippingName = personName.trim();
+console.log(strippingName);
---
+++ Q6StrippingNames/main.ts
@@ -0,0 +1,6 @@
+// Assignment # Stripping Names: 
+let personName : string ="\tAli abdullah khatib\n";
+console.log(personName);
+
+let strippingName : string = personName.trim();
+console.log(strippingName);
