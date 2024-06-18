91dd0f9d31d720fe0105212ccd17bf3ed841e76b
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-18 22:54:57 +0500

    first commit

---
+++ Q9FavoriteNumber/main.js
@@ -0,0 +1,4 @@
+//Assignment # 9
+var favorite_Number = 9;
+var message = "My favorite number is : ";
+console.log("".concat(message, " ").concat(favorite_Number));
---
+++ Q9FavoriteNumber/main.ts
@@ -0,0 +1,6 @@
+//Assignment # 9
+let favorite_Number : number = 9;
+
+let message : string = "My favorite number is : ";
+
+console.log(`${message} ${favorite_Number}`);
