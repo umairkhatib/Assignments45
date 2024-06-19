8d7aec008b9a4ad8c9fcb947f8edd7e5f4773c7c
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-19 11:49:17 +0500

    11th commit

---
+++ Q11Names/main.js
@@ -0,0 +1,7 @@
+//Asssignment # 11
+var names = ["Ali", "ahmed", "umar", "fatima"];
+// inddex              0        1     2        3
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+;
---
+++ Q11Names/main.ts
@@ -0,0 +1,7 @@
+//Asssignment # 11
+let names: string[]= ["Ali","ahmed","umar","fatima"];
+// inddex              0        1     2        3
+for (let i = 0; i < names.length; i++) {
+   console.log(names[i])
+    
+};
