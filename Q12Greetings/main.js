5289cb603647174bd1ec36607f58792930cfb674
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-19 12:28:05 +0500

    12th commit

---
+++ Q12Greetings/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Assignment # 12
+let personName = ["Awais", "Faisal", "shamoo", "Zar"];
+let message = "Asalamalikum , Hope you are doing well";
+//index                        0          1        2       3
+for (let i = 0; i < personName.length; i++) {
+    console.log(personName[i], message);
+}
+;
---
+++ Q12Greetings/main.ts
@@ -0,0 +1,12 @@
+//Assignment # 12
+
+let personName : string[] = ["Awais", "Faisal","shamoo","Zar"];
+let message : string= "Asalamalikum , Hope you are doing well";
+//index                        0          1        2       3
+for (let i = 0; i < personName.length; i++) {
+console.log(personName[i],message);
+    
+};
+
+
+
