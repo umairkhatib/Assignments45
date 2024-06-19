e0ec4bc14bb51873b0fc35b438cee571e27de09f
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-19 12:56:39 +0500

    13th commit

---
+++ Q13Array/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Assignemnt # 13
+let vehical = ["car", "motorcycle", "bus"];
+let message = "I would like to own a";
+for (let i = 0; i < vehical.length; i++) {
+    console.log(message, vehical[i]);
+}
---
+++ Q13Array/main.ts
@@ -0,0 +1,7 @@
+//Assignemnt # 13
+
+let vehical : string[] =["car" , "motorcycle" , "bus"];
+let message : string="I would like to own a";
+for (let i = 0; i < vehical.length; i++) {
+    console.log(message,vehical[i]);
+}
