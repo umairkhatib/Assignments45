fb94fc34d07176be688111a6667c6b59fa1bf27f
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-20 13:23:27 +0500

    14th commit

---
+++ Q14GuestList/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment # 14
+let personName = ["shamoo", "Fysal", "Irfan", "Awais"];
+// index                        0         1         2         3
+let invatation = (": You are invited for dinner for coming sunday");
+for (let i = 0; i < personName.length; i++) {
+    console.log(personName[i], invatation);
+}
---
+++ Q14GuestList/main.ts
@@ -0,0 +1,11 @@
+//Assignment # 14
+
+let personName : string[] = ["shamoo", "Fysal" , "Irfan" , "Awais"];
+// index                        0         1         2         3
+
+
+let invatation : string = (": You are invited for dinner for coming sunday");
+
+for (let i = 0; i < personName.length; i++) {
+    console.log(personName[i] , invatation )
+}
