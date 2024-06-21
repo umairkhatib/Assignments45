b00258b4ae6625eae81600053671ebf01d7b691f
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-22 04:10:10 +0500

    18th commit

---
+++ Q18FavoriteLocation/main.ts
@@ -0,0 +1,25 @@
+//Assignment # 18
+let favorite_location : string[]= ["Canada","America", "Bangladesh", "England","Dominica"];
+console.log("Original Order ",favorite_location);
+
+//Printing in alphabatic order
+console.log("Alphabatic Order",[...favorite_location].sort());
+
+console.log("Original Order ",favorite_location);
+
+//Printing in reverse alphabatic order
+console.log("Reverse Alphabatic Order",[...favorite_location].sort().reverse());
+
+console.log("Original Order ",favorite_location);
+
+//Reversing the order of list
+console.log("Reverse Original Order ",favorite_location.reverse());
+
+//Reversing back the order of list
+console.log("Reverse back Original Order ",favorite_location.reverse());
+
+//Printing in alphabatic order
+console.log("Alphabatic Order",[...favorite_location].sort());
+
+//Printing in reverse alphabatic order
+console.log("Reverse Alphabatic Order",[...favorite_location].sort().reverse());
