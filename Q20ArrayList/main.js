4294543160dfa06c8619df8bf5eded54eadfa504
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-24 00:30:06 +0500

    20th commit

---
+++ Q20ArrayList/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Assigment # 20
+let programing = ["GW basic", "Pascal", "C #", "Asp.net", "Typescript"];
+programing.forEach(programming_languages => {
+    console.log(programming_languages);
+});
---
+++ Q20ArrayList/main.ts
@@ -0,0 +1,5 @@
+//Assigment # 20
+let programing :string[]= ["GW basic", "Pascal", "C #", "Asp.net", "Typescript"];
+programing.forEach(programming_languages => {console.log(programming_languages)
+    
+});
