dd2dfbe60c91a0bd3e8635c7121ce79fabf43f8a
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-23 23:59:58 +0500

    19th commit

---
+++ Q19Numberofguest/main.js
@@ -0,0 +1,32 @@
+"use strict";
+//Assignment # 19
+let personName = ["Omer", "Fatima", "Ali", "Kamran"];
+//Index                        0       1        2        3
+let absentGuest = "kamran";
+let newGuest = "Farrukh";
+personName[3] = newGuest;
+personName.unshift("Noman");
+personName.splice(3, 0, "Farhan");
+personName.push("Umair");
+//Removing guests
+//console.log("Sorry ! Due to Bigger table unavailibilty We can only invite 2 Guests");
+while (personName.length > 2) {
+    let removeGuest = personName.pop();
+    //console.log(removeGuest , ": Sorry you are not invited for Dinner");    
+} /*
+//Printing blank Line
+console.log();
+
+let inviteMessage:string=": You are still invited for dinner On coming sunday"
+
+for (let i = 0; i < personName.length; i++) {
+    console.log(personName[i] , inviteMessage);
+}
+
+//Printing blank Line
+console.log();*/
+//Removing remaining 2 guests
+personName.splice(0, 2);
+//console.log(personName);
+//Printing total number of quests
+console.log("Total numbers of gruest remaing : ", personName.length);
