72fa994887d59f73d8d3e69c2a1b84c4bed52fe7
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-26 05:14:47 +0500

    22 commit

---
+++ Q22IntentionalError/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment # 22
+//Intentional Errot
+let personName = ["Kamran", "Farhan", "Zufiqar", "Mohsin"];
+//Index                      0         1        2          3
+console.log(personName[6]);
+//correcting intertional Error
+console.log(personName[1]);
---
+++ Q22IntentionalError/main.ts
@@ -0,0 +1,10 @@
+//Assignment # 22
+//Intentional Errot
+
+let personName:string[]=["Kamran","Farhan", "Zufiqar", "Mohsin"];
+//Index                      0         1        2          3
+
+console.log(personName[6]);
+
+//correcting intertional Error
+console.log(personName[1]);
