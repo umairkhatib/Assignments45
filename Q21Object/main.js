c2173f96b7d3bc04419de6ae1fc423f1ff2ea677
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-06-26 05:00:50 +0500

    21st commit

---
+++ Q21Object/main.js
@@ -0,0 +1,10 @@
+"use strict";
+//Assignment # 21
+//interface
+let vehicle = {
+    Brand: "Honda",
+    Model: "Civic",
+    Year: 2024,
+    Colour: "white"
+};
+console.log(vehicle);
---
+++ Q21Object/main.ts
@@ -0,0 +1,17 @@
+//Assignment # 21
+//interface
+
+interface vehicle{
+    Brand:string;
+    Model:string;
+    Year: number;
+    Colour: string;
+}
+
+let vehicle ={
+    Brand:"Honda",
+    Model:"Civic",
+    Year:2024,
+    Colour:"white"
+}
+console.log(vehicle);
