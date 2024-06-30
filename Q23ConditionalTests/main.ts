21c533c8912d234867f89670edccb8eb342f2a77
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-01 03:23:19 +0500

    23rd commit

---
+++ Q23ConditionalTests/main.ts
@@ -0,0 +1,29 @@
+//Assigment # 23
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+console.log("Is car !== 'subaru'? I predict false.");
+console.log(car !== 'subaru');
+
+console.log("Is car == 'honda'? I predict false.");
+console.log(car == 'honda');
+console.log("Is car !== 'honda'? I predict True.");
+console.log(car !== 'honda');
+
+console.log("Is car == 'toyota'? I predict false.");
+console.log(car == 'toyota');
+console.log("Is car !== 'toyota'? I predict True.");
+console.log(car !== 'toyota');
+
+console.log("Is car == 'BMW'? I predict false.");
+console.log(car == 'BMW');
+console.log("Is car !== 'BMW'? I predict True.");
+console.log(car !== 'BMW');
+
+console.log("Is car == 'Mercedes'? I predict false.");
+console.log(car == 'Mercedes');
+console.log("Is car !== 'Mercedes'? I predict True.");
+console.log(car !== 'Mercedes');
+
+
