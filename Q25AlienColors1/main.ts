7ea8b5aba4f18470c05ceb8380c9779c6e4815fc
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-06 03:36:53 +0500

    25th commit

---
+++ Q25AlienColors1/main.ts
@@ -0,0 +1,15 @@
+//Assignment # 25
+
+import inquirer from "inquirer";
+
+let answer = await inquirer.prompt({
+    name: "color",
+    message: "Guess Alien Color"
+
+})
+if (answer.color ==="Green"){
+    console.log("the player just earned 5 points")
+}
+if (answer.color =="blue"){
+    console.log();
+}
