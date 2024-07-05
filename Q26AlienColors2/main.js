c829d2aa297466aa59e0896867e711b2297873d8
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-06 04:01:05 +0500

    26th commit

---
+++ Q26AlienColors2/main.js
@@ -0,0 +1,11 @@
+import inquirer from "inquirer";
+let answer = await inquirer.prompt({
+    name: "color",
+    message: "Guess Alien Color"
+});
+if (answer.color === "Green") {
+    console.log("the player just earned 5 points");
+}
+else {
+    console.log("player just earned 10 points.");
+}
