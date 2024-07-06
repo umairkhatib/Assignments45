a0be11752ebef5da00b8561ea26eeab2d6b0118c
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-06 05:47:12 +0500

    28th commit

---
+++ Q28StageOfLife/main.js
@@ -0,0 +1,25 @@
+// Assignment # 28
+import inquirer from "inquirer";
+let answer = await inquirer.prompt({
+    name: "age",
+    type: "number",
+    message: "What is your age : "
+});
+if (answer.age < 2) {
+    console.log("the person is a baby");
+}
+else if (answer.age >= 2 && answer.age < 4) {
+    console.log("the person is a toddler");
+}
+else if (answer.age >= 4 && answer.age < 13) {
+    console.log("the person is a kid");
+}
+else if (answer.age >= 13 && answer.age < 20) {
+    console.log("person is a teenager");
+}
+else if (answer.age >= 20 && answer.age < 65) {
+    console.log("the person is an adult");
+}
+else if (answer.age >= 65) {
+    console.log("the person is an elder");
+}
