9e065a6b5e6379e2ae1711d42ab6ea210ef338d5
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-09 00:43:10 +0500

    34th commit

---
+++ Q34Pizza/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Assignmrnt # 34
+let pizza = ["Fajita", "Margherita", "Pepperoni", "ChickenTikka"];
+for (let i = 0; i < pizza.length; i++) {
+    const pizzas = pizza[i];
+    console.log(`I like ${pizzas} Pizza`);
+}
+console.log();
+console.log("I Really love Pizzas");
---
+++ Q34Pizza/main.ts
@@ -0,0 +1,8 @@
+//Assignmrnt # 34
+let pizza : string[] =["Fajita","Margherita", "Pepperoni", "ChickenTikka"];
+for (let i = 0; i < pizza.length; i++) {
+    const pizzas = pizza[i];
+    console.log(`I like ${pizzas} Pizza`);
+}
+console.log();
+console.log("I Really love Pizzas");
