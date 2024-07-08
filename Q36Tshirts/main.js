4430e41df465193ab6c8d80532b05742a5aa2492
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-09 02:35:58 +0500

    36th commit

---
+++ Q36Tshirts/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assignment # 36
+function Tshirts(size, printmessage) {
+    console.log(`SIZE : ${size} ,${printmessage}`);
+}
+Tshirts("small", "Umair Aziz");
+Tshirts("Medium", "Shahid Afridi");
+Tshirts("Large", "Shoib Akhtar");
---
+++ Q36Tshirts/main.ts
@@ -0,0 +1,7 @@
+//Assignment # 36
+function Tshirts(size:string,printmessage:string) {
+    console.log(`SIZE : ${size} ,${printmessage}`);
+}
+Tshirts("small","Umair Aziz");
+Tshirts("Medium","Shahid Afridi");
+Tshirts("Large","Shoib Akhtar");
