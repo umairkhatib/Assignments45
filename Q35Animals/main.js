c77e584c6a4a3b9aceca7c7803259b75d6e1674e
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-09 00:58:29 +0500

    35th commit

---
+++ Q35Animals/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Assignment # 35
+let animal = ["Cat", "Dog", "loin"];
+for (let i = 0; i < animal.length; i++) {
+    console.log(`A ${animal[i]} would make a great pet.`);
+}
+console.log();
+console.log("Any of these animals would make a great pet!");
---
+++ Q35Animals/main.ts
@@ -0,0 +1,10 @@
+import { Console } from "console";
+
+// Assignment # 35
+let animal:string[]=["Cat", "Dog", "loin"];
+
+for (let i = 0; i < animal.length; i++) {
+    console.log(`A ${animal[i]} would make a great pet.`);    
+}
+console.log();
+console.log("Any of these animals would make a great pet!");
