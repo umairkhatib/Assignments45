08e74d99e29d61ca45f3fdb3c83c9133d24b063d
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-09 04:32:15 +0500

    37th commit

---
+++ Q37Largeshirt/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assigment # 37
+function make_shirt(size = "large", printMessage = "I Love typescript") {
+    console.log(`SIZE : ${size} ,${printMessage}`);
+}
+make_shirt("small", "I love Pakistan");
+make_shirt("Medium");
+make_shirt();
---
+++ Q37Largeshirt/main.ts
@@ -0,0 +1,8 @@
+//Assigment # 37
+function make_shirt(size:string="large",printMessage:string="I Love typescript") {
+    console.log(`SIZE : ${size} ,${printMessage}`);
+    
+}
+make_shirt("small","I love Pakistan");
+make_shirt("Medium");
+make_shirt();
