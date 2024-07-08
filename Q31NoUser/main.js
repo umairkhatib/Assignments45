960c7e9ec4ef35800f173b14061d533198e64bc0
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-08 10:13:15 +0500

    31st commit

---
+++ Q31NoUser/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Assignment # 31
+let userName = ["admin", "umair", "ali", "fatima", "omer"];
+console.log("Array lenght :", userName.length);
+// Removing users from Array
+userName = [];
+console.log("We need to find some users!", userName.length);
---
+++ Q31NoUser/main.ts
@@ -0,0 +1,7 @@
+//Assignment # 31
+let userName : string[] = ["admin" , "umair" , "ali", "fatima", "omer"];
+console.log("Array lenght :",userName.length);
+
+// Removing users from Array
+userName = [];
+console.log("We need to find some users!",userName.length);
