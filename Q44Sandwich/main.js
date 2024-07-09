2e1b4bb43e4cc8d095569e877cc832a2c56a7d85
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-10 03:01:17 +0500

    44th commit

---
+++ Q44Sandwich/main.js
@@ -0,0 +1,9 @@
+"use strict";
+//Assignment # 45
+function sandwich(arr) {
+    console.log("\nYour order is ready using below items");
+    console.log(`${arr}`);
+}
+sandwich(["item1", "item2", "item3"]);
+sandwich(["item4", "item5", "item6"]);
+sandwich(["item7", "item8", "item9"]);
---
+++ Q44Sandwich/main.ts
@@ -0,0 +1,8 @@
+//Assignment # 45
+function sandwich(arr:string[]){
+   console.log("\nYour order is ready using below items");
+   console.log(`${arr}`);
+}
+sandwich(["item1","item2","item3"]);
+sandwich(["item4","item5","item6"]);
+sandwich(["item7","item8","item9"]);
