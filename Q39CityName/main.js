125b42b7eae6ad0f0ea7219d42fd03013b62463d
unknown <Administrator@DESKTOP-4KKJSPC.int.sybrid.com>
2024-07-09 05:01:24 +0500

    39th commit

---
+++ Q39CityName/main.js
@@ -0,0 +1,8 @@
+"use strict";
+//Assigment # 39
+function city_country(cityname, country) {
+    return (`${cityname} , ${country}`);
+}
+console.log(city_country("Karachi", "Pakistan"));
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("Colombo", "Srilanka"));
---
+++ Q39CityName/main.ts
@@ -0,0 +1,8 @@
+//Assigment # 39
+function city_country(cityname:string,country:string): string {
+    return(`${cityname} , ${country}`);
+    
+}
+console.log(city_country("Karachi","Pakistan"));
+console.log(city_country("Lahore","Pakistan"));
+console.log(city_country("Colombo","Srilanka"));
